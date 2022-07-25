a6f395ab016ec753c0c8cd3a14f520202be2acb6
Agnese Brudere <agnesebrudere@gmail.com>
2022-07-25 16:31:44 +0300

    SecondActivity

---
+++ src/com/bootcampexcercise/module2/activity/OpratorActivity.java
@@ -0,0 +1,18 @@
+package com.bootcampexcercise.module2.activity;
+
+public class OpratorActivity {
+    public static void main(String[] args) {
+        int val1 = 12;
+        int val2 = 5;
+
+        System.out.println("val1 + val2 = " + (val1 + val2));
+
+        System.out.println("val1 - val2 = " + (val1 - val2));
+
+        System.out.println("val1 * val2 = " + (val1 * val2));
+
+        System.out.println("val1 / val2 = " + (val1 / val2));
+
+        System.out.println("val1 % val2 = " + (val1 % val2));
+    }
+}
