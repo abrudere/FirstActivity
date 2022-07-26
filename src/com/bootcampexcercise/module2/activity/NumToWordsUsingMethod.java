451d8de3105dd2a5c5659e771fc45637ca534de0
Agnese Brudere <agnesebrudere@gmail.com>
2022-07-26 11:51:06 +0300

    Activity nine

---
+++ src/com/bootcampexcercise/module2/activity/NumToWordsUsingMethod.java
@@ -0,0 +1,46 @@
+package com.bootcampexcercise.module2.activity;
+
+public class NumToWordsUsingMethod {
+        public static void main(String args[]) {
+            for(int i = 1; i <= 10; i++) {
+                numToWordsLogic(i);
+            }
+        }
+        public static void numToWordsLogic(int x) {
+            switch(x) {
+                case 1:
+                    System.out.println("The number is " + x);
+                    break;
+                case 2:
+                    System.out.println("The number is " + x);
+                    break;
+                case 3:
+                    System.out.println("The number is " + x);
+                    break;
+                case 4:
+                    System.out.println("The number is " + x);
+                    break;
+                case 5:
+                    System.out.println("The number is " + x);
+                    break;
+                case 6:
+                    System.out.println("The number is " + x);
+                    break;
+                case 7:
+                    System.out.println("The number is " + x);
+                    break;
+                case 8:
+                    System.out.println("The number is " + x);
+                    break;
+                case 9:
+                    System.out.println("The number is " + x);
+                    break;
+                case 10:
+                    System.out.println("The number is " + x);
+                    break;
+                default:
+                    System.out.println("Error");
+            }
+
+    }
+}
