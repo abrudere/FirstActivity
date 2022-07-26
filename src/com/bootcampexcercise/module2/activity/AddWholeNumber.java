ef7dce4da268192f8e6db4a92f378773a33acccd
Agnese Brudere <agnesebrudere@gmail.com>
2022-07-26 09:19:48 +0300

    Activity five

---
+++ src/com/bootcampexcercise/module2/activity/AddWholeNumber.java
@@ -0,0 +1,11 @@
+package com.bootcampexcercise.module2.activity;
+
+public class AddWholeNumber {
+    public static void main(String args[]) {
+
+        for(int i = 50;i <= 100; i++){
+            System.out.print(i + " ");
+        }
+
+    }
+}
